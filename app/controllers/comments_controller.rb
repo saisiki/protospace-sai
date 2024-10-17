98db54886abcace0cde1b5cab1f765f0cc43b4ba
saisiki <yumi.fukumoto911110@gmail.com>
2024-10-17 18:16:53 +0900

    修正４

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,10 +2,10 @@ class CommentsController < ApplicationController
   def create
     @prototype = Prototype.find(params[:prototype_id])
     @comment = @prototype.comments.build(comment_params)
+
     if @comment.save
       redirect_to prototype_path(@prototype)
     else
-      @comments = @prototype.comments.includes(:user)
       render 'prototypes/show', status: :unprocessable_entity
     end
   
