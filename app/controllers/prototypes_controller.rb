5a67c24ba77148a88562887eb3aceb019590b6df
saisiki <yumi.fukumoto911110@gmail.com>
2024-10-17 16:54:59 +0900

    initial commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,65 @@
+class PrototypesController < ApplicationController
+  before_action :move_to_index, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+
+
+  def index
+    @prototypes = Prototype.all  # 既存のコード
+    @prototype = Prototype.new   # 新規作成フォーム用
+  end
+
+  def new
+    @prototype = Prototype.new
+    
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path, notice: 'プロトタイプが正常に作成されました。'
+    else
+      puts "Prototype errors: #{@prototype.errors.full_messages}"
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+    
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype)
+    else 
+      render :edit, status: :unprocessable_entity 
+    end
+  end
+
+  def destroy
+    @prototype = Prototype.find(params[:id])
+    @prototype.destroy
+    redirect_to root_path
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+
+  end
+
+  def move_to_index
+    @prototype = Prototype.find(params[:id])
+    unless current_user == @prototype.user
+      redirect_to action: :index, alert: '権限がありません。'
+    end
+
+  end
+end
