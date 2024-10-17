b36542a874f29cd0c4413e116cc3762d764ab28b
saisiki <yumi.fukumoto911110@gmail.com>
2024-10-17 18:15:24 +0900

    syuusei4 ?

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,10 +2,10 @@ class CommentsController < ApplicationController
   def create
     @prototype = Prototype.find(params[:prototype_id])
     @comment = @prototype.comments.build(comment_params)
-    @comment.user = current_user
     if @comment.save
       redirect_to prototype_path(@prototype)
     else
+      @comments = @prototype.comments.includes(:user)
       render 'prototypes/show', status: :unprocessable_entity
     end
   
@@ -13,6 +13,6 @@ class CommentsController < ApplicationController
 
   private 
   def comment_params
-    params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+    params.require(:comment).permit(:content).merge(user_id: current_user.id)
   end
 end
